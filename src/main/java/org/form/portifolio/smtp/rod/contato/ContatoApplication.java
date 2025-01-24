387878fd662cc3a53b80c76d7371c566a4d787e1
Rodger <rodgerdias6@gmail.com>
2025-01-24 13:34:51 -0300

    init repository

---
+++ src/main/java/org/form/portifolio/smtp/rod/contato/ContatoApplication.java
@@ -0,0 +1,13 @@
+package org.form.portifolio.smtp.rod.contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/form/portifolio/smtp/rod/contato/ContatoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.form.portifolio.smtp.rod.contato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
